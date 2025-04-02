321994c16ea0c4b2f9bd4c4ebaa2d2d8222e25e8
sajinael98 <saji.nael.98@gmail.com>
2025-04-03 01:56:45 +0300

    add key to Fragment compoent in Menu dashboard

--- src/app/(admin)/app/components/admin-panel/menu/index.tsx
+++ src/app/(admin)/app/components/admin-panel/menu/index.tsx
@@ -3,44 +3,29 @@
 import { NavLink, ScrollArea, Text } from "@mantine/core";
 import { useMenu } from "@refinedev/core";
 import Link from "next/link";
+import { Fragment } from "react";
 
 export const Menu = () => {
   const { menuItems } = useMenu();
 
   const items = menuItems.map(({ key, label, icon, list, children }, index) => {
-    const commonProps = {
-      label: label,
-      leftSection: icon,
-    };
-
-    if (children.length) {
-      return (
-        <>
-          <Text fw={500} c="dimmed" fz="sm" mt={index === 0 ? undefined : "sm"}>
-            {label}
-          </Text>
-          {children.map((child) => (
-            <NavLink
-              key={child.identifier}
-              href={child?.list?.toString() ?? "/"}
-              component={Link}
-              leftSection={child.icon}
-              label={child.label}
-              fz="sm"
-            />
-          ))}
-        </>
-      );
-    } else {
-      return (
-        <NavLink
-          key={key}
-          href={list?.toString() ?? ""}
-          component={Link}
-          {...commonProps}
-        />
-      );
-    }
+    return (
+      <Fragment key={key}>
+        <Text fw={500} c="dimmed" fz="sm" mt={index === 0 ? undefined : "sm"}>
+          {label}
+        </Text>
+        {children.map((child) => (
+          <NavLink
+            key={child.label}
+            href={child?.list?.toString() ?? "/"}
+            component={Link}
+            leftSection={child.icon}
+            label={child.label}
+            fz="sm"
+          />
+        ))}
+      </Fragment>
+    );
   });
 
   return <ScrollArea>{items}</ScrollArea>;
